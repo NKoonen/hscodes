14bf9afec550e5c771e90eedf1141a9661865d9c
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2021-09-01 13:36:36 +0200

    Enhance import overwrite

--- override/controllers/admin/AdminImportController.php
+++ override/controllers/admin/AdminImportController.php
@@ -5,8 +5,11 @@ class AdminImportController extends AdminImportControllerCore
 	public function __construct() {
 		parent::__construct();
 
-		if ( isset( $this->available_fields['id_product'] ) && isset( $this->available_fields['reference'] ) ) {
-			$this->available_fields['hscode'] = 'HS Code';
+		if ( isset( $this->available_fields['id'] ) && isset( $this->available_fields['reference'] ) ) {
+			$this->available_fields['hscode'] = array(
+				'label' => $this->trans('HS Code'),
+				'help' => $this->trans('HS Code')
+			);
 		}
 	}
 }
