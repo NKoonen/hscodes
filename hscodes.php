f60543c053b8dd268332f495cee397fce386f6cd
Inform-All <NKoonen@users.noreply.github.com>
2023-01-02 08:52:29 +0100

    minor version update

--- hscodes.php
+++ hscodes.php
@@ -10,7 +10,7 @@ class Hscodes extends Module
     {
         $this->name = 'hscodes';
         $this->tab = 'shipping_logistics';
-        $this->version = '1.0.0';
+        $this->version = '1.0.1';
         $this->author = 'Inform-All';
         $this->need_instance = 0;
         $this->bootstrap = true;
